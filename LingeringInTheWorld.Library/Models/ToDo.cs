75325e60729d5fdd6e8ab5579ccc84a4dc9e3038
aoao1024 <20226772@stu.neu.edu.cn>
2024-11-14 16:20:25 +0800

    增加代办实体类、数据库的读写操作

---
+++ LingeringInTheWorld.Library/Models/ToDo.cs
@@ -0,0 +1,12 @@
+namespace LingeringInTheWorld.Library.Models;
+
+[SQLite.Table("todo")]
+public class ToDo
+{
+    [SQLite.Column("id")] public int Id { get; set; }
+    [SQLite.Column("title")] public string Title { get; set; }=string.Empty;
+    [SQLite.Column("content")]
+    public string Content { get; set; } = string.Empty;
+    [SQLite.Column("status")] public int Status { get; set; }
+    [SQLite.Column("deadline")] public DateTime DeadLine { get; set; }
+}
---
+++ LingeringInTheWorld.Library/Services/ToDoStorage.cs
@@ -0,0 +1,54 @@
+using System.Linq.Expressions;
+using LingeringInTheWorld.Library.Helpers;
+using LingeringInTheWorld.Library.Models;
+using SQLite;
+namespace LingeringInTheWorld.Library.Services;
+public class ToDoStorage : IToDoStorage
+{
+    public const string DbName = "todo.sqlite3";
+    public static readonly string TodoDbPath = PathHelper.GetLocalFilePath(DbName);
+    private SQLiteAsyncConnection _connection;
+    //public bool IsInitialized => File.Exists(TodoDbPath);
+    private SQLiteAsyncConnection Connection =>
+        _connection ??= new SQLiteAsyncConnection(TodoDbPath);
+    public async Task<int> AddToDoItemAsync(ToDo toDo) 
+        => await Connection.InsertAsync(toDo);
+    
+    public async Task<int> DeleteToDoItemAsync(int id)
+    =>await Connection.DeleteAsync<ToDo>(id);
+
+    public async Task DeleteToDoItemsAsync(int[] ids)
+    {
+        await Connection.RunInTransactionAsync(async (transaction) =>
+        {
+            try
+            {
+                foreach (var id in ids)
+                    await DeleteToDoItemAsync(id);
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                transaction.Rollback();
+            }
+        });
+        
+    }
+    public async Task UpdateToDoItemAsync(int id, object deadline=null, object title = null, object content = null)
+    {
+        var toDo =await Connection.FindAsync<ToDo>(id);
+        if (deadline!=null)
+        {
+            toDo.DeadLine = (DateTime)deadline;
+        }
+
+        if (title != null) toDo.Title = (string)title;
+        if (content != null) toDo.Content = (string)content;
+        await Connection.UpdateAsync(toDo);
+    }
+
+    public async Task<IList<ToDo>> GetTodoListAsync(Expression<Func<ToDo, bool>> where, int skip, int take)
+        => await Connection.Table<ToDo>().Where(where).Skip(skip).Take(take).ToListAsync();
+
+}
