cb6480b5d8823610476095f7a2b87c18d8e16ab6
aoao1024 <20226772@stu.neu.edu.cn>
2024-11-15 19:40:26 +0800

    增加显示实时天气功能，还不能定位:(

--- LingeringInTheWorld.Library/ViewModels/DiaryAddViewModel.cs
+++ LingeringInTheWorld.Library/ViewModels/DiaryAddViewModel.cs
@@ -8,7 +8,22 @@ namespace LingeringInTheWorld.Library.ViewModels;
 
 public class DiaryAddViewModel : ViewModelBase
 {
+    
+    private readonly IAlertService _alertService;
+    private readonly HeFengWeatherService _weatherService;
+    // private readonly LocationService _locationService;
+    
     private string _currentTime;
+    private string _currentWeatherCondition;  // 天气状况
+    
+    public DiaryAddViewModel(IAlertService alertService)
+    {
+        _alertService = alertService;
+        CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        _weatherService = new HeFengWeatherService(alertService);
+        // _locationService = new LocationService(alertService);
+        GetWeatherByLocation();
+    }
     
     public string CurrentTime
     {
@@ -20,9 +35,10 @@ public class DiaryAddViewModel : ViewModelBase
         }
     }
     
-    public DiaryAddViewModel()
+    public string CurrentWeatherCondition
     {
-        CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        get => _currentWeatherCondition;
+        set => SetProperty(ref _currentWeatherCondition, value);
     }
     
     public event PropertyChangedEventHandler PropertyChanged;
@@ -30,5 +46,21 @@ public class DiaryAddViewModel : ViewModelBase
     protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    
+    public async void GetWeatherByLocation()
+    {
+        // 获取当前位置经纬度
+        // var (latitude, longitude) = await _locationService.GetLocationAsync();
+        var latitude = 42.67;
+        var longitude = 123.46;
+
+        var weatherInfo = await _weatherService.GetWeatherByLocationAsync(latitude, longitude);
+
+        if (weatherInfo != null)
+        {
+            var condition = weatherInfo.Condition;
+            CurrentWeatherCondition = $"{condition}";
+        }
     }
 }
