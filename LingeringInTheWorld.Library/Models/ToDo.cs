b34dc0a81040caa2a2094370c998e51029c6c179
monimonicaca <guanjie18@163.com>
2024-11-16 15:59:41 +0800

    id自增

--- LingeringInTheWorld.Library/Models/ToDo.cs
+++ LingeringInTheWorld.Library/Models/ToDo.cs
@@ -1,18 +1,13 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using SQLite;
 
 namespace LingeringInTheWorld.Library.Models;
 
 // [SQLite.Table("todos")]
 public class ToDo :ObservableObject
 {
-    // [SQLite.Column("id")] public int Id { get; set; }
-    // [SQLite.Column("title")] public string Title { get; set; }=string.Empty;
-    // [SQLite.Column("content")]
-    // public string Content { get; set; } = string.Empty;
-    // [SQLite.Column("status")] public int Status { get; set; }
-    // [SQLite.Column("deadline")] public DateTime DeadLine { get; set; }
     
-    //1115
+    [PrimaryKey, AutoIncrement]
      public int Id { get; set; }
      public string Title { get; set; }=string.Empty;
     
